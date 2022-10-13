bfe93a508d7a47f06b170e7066829d4d53193d17
Parham Saremi <parhaamsaremi@gmail.com>
2022-10-13 12:23:54 +0330

    WIP1: add try withs

--- NOnion/Services/TorServiceClient.fs
+++ NOnion/Services/TorServiceClient.fs
@@ -87,39 +87,50 @@ type TorServiceClient =
                                     do!
                                         circuit.Extend randomMiddleNode
                                         |> Async.Ignore
-
-                                    do!
-                                        circuit.Extend hsDirectoryNode
-                                        |> Async.Ignore
+                                    try
+                                        do!
+                                            circuit.Extend hsDirectoryNode
+                                            |> Async.Ignore
+                                    with
+                                    | :? NOnionException ->
+                                         return! downloadDescriptor tail
 
                                     let dirStream = TorStream circuit
 
-                                    do!
-                                        dirStream.ConnectToDirectory()
-                                        |> Async.Ignore
+                                    try
+                                        do!
+                                            dirStream.ConnectToDirectory()
+                                            |> Async.Ignore
+                                    with
+                                    | :? NOnionException ->
+                                         return! downloadDescriptor tail
+                                    
+                                    try
+                                        let! documentInString =
+                                            TorHttpClient(
+                                                dirStream,
+                                                Constants.DefaultHttpHost
+                                            )
+                                                .GetAsString
+                                                (sprintf
+                                                    "/tor/hs/%i/%s"
+                                                    Constants.HiddenServices.Version
+                                                    ((Convert.ToBase64String
+                                                        blindedPublicKey)))
+                                                false
 
-                                    let! documentInString =
-                                        TorHttpClient(
-                                            dirStream,
-                                            Constants.DefaultHttpHost
-                                        )
-                                            .GetAsString
-                                            (sprintf
-                                                "/tor/hs/%i/%s"
-                                                Constants.HiddenServices.Version
-                                                ((Convert.ToBase64String
-                                                    blindedPublicKey)))
-                                            false
-
-                                    return
-                                        HiddenServiceFirstLayerDescriptorDocument.Parse
-                                            documentInString
+                                        return
+                                            HiddenServiceFirstLayerDescriptorDocument.Parse
+                                                documentInString
+                                    with
+                                    | :? NOnionException ->
+                                         return! downloadDescriptor tail
 
                                 with
                                 | :? NOnionException ->
                                         // Using micro descriptors means we might use servers that are hibernating or etc
                                         // so we need to be able to try multiple servers to receive the descriptor.
-                                    return! downloadDescriptor tail
+                                    return! downloadDescriptor responsibleDirs
                         }
 
                     let! firstLayerDescriptorDocument =
