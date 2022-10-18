aaf2affd08f14770a6923b1567328fc885aed7db
Afshin Arani <afshin@arani.dev>
2022-10-18 12:20:04 +0330

    Network, Utility: move simple result impl
    This commit moves our simple Result implementation
    to the Utility namespace so we can freely use it wherever
    we need it.

---
+++ NOnion/Utility/ResultUtil.fs
@@ -0,0 +1,9 @@
+﻿namespace NOnion.Utility
+
+// RequireQualifiedAccess is needed to prevent collision with
+// Failure function that creates general exceptions
+
+[<RequireQualifiedAccess>]
+type OperationResult<'T> =
+    | Ok of 'T
+    | Failure of exn
